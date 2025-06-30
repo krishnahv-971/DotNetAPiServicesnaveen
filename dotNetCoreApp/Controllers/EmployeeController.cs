7e2ff7006e6a176776a4b322b382b6aa55fe0f74
Naveen V <naveenv@Relyon.com>
2025-06-30 11:59:35 +0530

    Add EmployeeController and related changes

--- dotNetCoreApp/Controllers/EmployeeController.cs
+++ dotNetCoreApp/Controllers/EmployeeController.cs
@@ -1,26 +1,49 @@
 using dotNetCoreApp.DataBase;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotNetCoreApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeeController : ControllerBase
+    public class EmployeeController(AppDbContext context) : ControllerBase
     {
-        private readonly AppDbContext _context;
-
-        public EmployeeController(AppDbContext context)
-        {
-            _context = context;
-        }
+        private readonly AppDbContext _context = context;
 
         [HttpGet("test")]
-        public IActionResult Test()
+        public async Task<IActionResult> Test()
         {
-            var details = _context.EmployeDetails.ToList();
+            // var details = await _context.EmployeDetails.ToListAsync();
+            return Ok("details");
+        }
 
-            return Ok(details);
+
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetAllEmployeeDetails()
+        {
+            var items = new List<EmployeeDetail>
+            {
+            new EmployeeDetail
+            {
+                Department = "IT",
+                Email = "john.doe@example.com",
+                EmployeeID = 1
+            },
+            new EmployeeDetail
+            {
+                Department = "HR",
+                Email = "jane.smith@example.com",
+                EmployeeID = 2
+            },
+            new EmployeeDetail
+            {
+                Department = "Finance",
+                Email = "mike.brown@example.com",
+                EmployeeID = 3
+            }
+            };
+
+            return Ok(items);
         }
     }
 }
